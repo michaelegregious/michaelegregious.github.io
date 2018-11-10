b163683abf7a95db54bd7f42173f1fa921040a1f
Michael Bush <lil.gordie@gmail.com>
2018-11-10 11:41:24 -0600

    Updates

---
+++ precache-manifest.a41830acbff3ce79f2d24d8249e9dc76.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb9e3bc62b1afd83d333fe6da4926dd3",
+    "url": "/static/media/urbanForagerScreencap.bb9e3bc6.png"
+  },
+  {
+    "revision": "286becab424905da90273c39bd1042e3",
+    "url": "/static/media/portfolioPic5.286becab.jpg"
+  },
+  {
+    "revision": "3c11fa702218d7dcb9bb3b53f48cd1eb",
+    "url": "/static/media/memenomicsScreencap.3c11fa70.png"
+  },
+  {
+    "revision": "97021545104ded7e38c2c1acfd3c18d6",
+    "url": "/static/media/graceShopperScreenCap.97021545.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "81691090f2baa6710649",
+    "url": "/static/js/main.81691090.chunk.js"
+  },
+  {
+    "revision": "db85c072ca7a7efdfdc4",
+    "url": "/static/js/1.db85c072.chunk.js"
+  },
+  {
+    "revision": "81691090f2baa6710649",
+    "url": "/static/css/main.2a16bca1.chunk.css"
+  },
+  {
+    "revision": "ba91dfb9688b7c55944770f04a4fdff9",
+    "url": "/index.html"
+  }
+];

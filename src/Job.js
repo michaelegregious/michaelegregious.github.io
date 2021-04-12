ee3fc8822afcc552bbeae6c07fa394a8f2d4ab06
michaelegregious <mbush@greenkeytech.com>
2021-04-12 13:41:48 -0500

    Adds Job

---
+++ src/Job.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Job = (props) => {
+  const { img, name, description, webUrl, gitUrl } = props;
+  return (
+    <div>
+      <b>GreenKey Technologies (GK)</b>
+      <i>Software Engineer</i> • Dec, 2018 — Feb, 2021
+      <ul>
+        <li>
+          Collaborated on a small, agile team to design and build full stack
+          applications utilizing modern Typescript, React, Node/Express, GoLang,
+          PostgreSQL, MongoDB, Docker, Redis.
+        </li>
+        <li>
+          Built, scrapped, and built again three iterations of GK's successful
+          Focus UI suite, improving customer satisfaction, UX, and data
+          model-training feedback dramatically.
+        </li>
+        <li>
+          Owned test coverage improvements. Achieved up to 95 percent throughout
+          applications.
+        </li>
+        <li>
+          Coordinated with the entire GK team (from Data Science to Engineering
+          and Customer Success) to iterate on rapidly-evolving needs of high
+          profile customers (incl. six Fortune 500 companies and five top tier
+          banks).
+        </li>
+        <li>
+          Researched and contributed meaningfully to story-driven UX processes,
+          incorporating real time SME feedback.
+        </li>
+        <li>
+          Spearheaded meaningful architectural restructuring dialog to ensure
+          scalability, readability and extensibility of codebases.
+        </li>
+      </ul>
+      <br />
+    </div>
+  );
+};
+
+export default SingleProject;
---
+++ src/Work-Experience.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Job from './Job'
+
+const WorkExperience = () => {
+  return (
+    <section class="section hero is-primary">
+      <div className="container content">
+        <h3 class="subhead-text">Work Experience</h3>
+        <br />
+        <Job />
+      </div>
+    </section>
+  );
+};
+
+export default Projects

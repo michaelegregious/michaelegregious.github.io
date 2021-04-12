89b0df1bf65cc17bd294106d7b7910a54d8bf9f8
michaelegregious <mbush@greenkeytech.com>
2021-04-12 14:31:11 -0500

    adds work exp

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Projects from './Projects';
 import Footer from './Footer';
 import './styles/main.scss';
 import { Element } from 'react-scroll';
+import WorkExperience from './Work-Experience';
 
 class App extends Component {
   render() {
@@ -15,6 +16,9 @@ class App extends Component {
         <ProfileHero />
         <Element name="about" className="element">
           <About />
+        </Element>
+        <Element name="work experience" className="element">
+          <WorkExperience/>
         </Element>
         <Element name="projects" className="element">
           <Projects />
